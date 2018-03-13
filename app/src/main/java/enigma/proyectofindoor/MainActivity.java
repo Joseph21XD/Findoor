211ab334a5c7649f6843bd364952264d17f6ea75
Joseph <ramirezjantonio@hotmail.com>
2018-03-12 23:07:50 -0600

    creacion de interfaz 3 activities

--- app/src/main/java/enigma/proyectofindoor/MainActivity.java
+++ app/src/main/java/enigma/proyectofindoor/MainActivity.java
@@ -1,9 +1,16 @@
 package enigma.proyectofindoor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void registrar(View view){
+        Intent intent= new Intent(MainActivity.this,InformacionActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
