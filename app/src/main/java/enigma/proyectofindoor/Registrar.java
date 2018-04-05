cfb1c97c4527e155e7055950a7151e2bf87b2da8
Joseph <ramirezjantonio@hotmail.com>
2018-04-05 13:59:56 -0600

    J-revisando push

--- app/src/main/java/enigma/proyectofindoor/Registrar.java
+++ app/src/main/java/enigma/proyectofindoor/Registrar.java
@@ -2,6 +2,7 @@ package enigma.proyectofindoor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Registrar extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Registrar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+    }
+
+    public void meCagoEnNavas(){
+        Log.d("ME CAGO","EN NAVAS");
     }
 }
